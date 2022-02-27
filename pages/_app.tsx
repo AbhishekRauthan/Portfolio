e8fec06739fb02858b7e6bf15bdabe5815874e73
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-02-27 17:06:28 +0530

    provided theme to Chakra Provider and wrapped component with layout element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import BaseStyles from "@components/baseStyles";
-import theme from '@components/Theme'
+import theme from "@components/Theme";
+import Layout from "@components/layout";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <ChakraProvider theme={theme}>
         <BaseStyles />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ChakraProvider>
     </>
   );
