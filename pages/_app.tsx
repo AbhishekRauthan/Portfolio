33c25b56981b6ef36cc42eea6d01a92f0c93b08f
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-24 22:48:35 +0530

    added page change animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,35 @@
 import type { AppProps } from "next/app";
+import { AnimatePresence, motion } from "framer-motion";
 import { ChakraProvider } from "@chakra-ui/react";
 import BaseStyles from "@components/baseStyles";
 import theme from "@components/theme";
 import Layout from "@components/layout";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <>
       <ChakraProvider theme={theme}>
         <BaseStyles />
         <Layout>
-          <Component {...pageProps} />
+          <AnimatePresence exitBeforeEnter initial={false}>
+            <motion.main
+              key={router.route}
+              initial="pageInitial"
+              animate="pageAnimate"
+              variants={{
+                pageInitial: {
+                  opacity: 0,
+                  translateY: 50,
+                },
+                pageAnimate: {
+                  opacity: 1,
+                  translateY: 0,
+                },
+              }}
+            >
+              <Component {...pageProps} />
+            </motion.main>
+          </AnimatePresence>
         </Layout>
       </ChakraProvider>
     </>
