f603fd854ab4ab0519d38ac3385c785796b76efb
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-24 15:45:17 +0530

    renamed folder to theme and updated import statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import BaseStyles from "@components/baseStyles";
-import theme from "@components/Theme";
+import theme from "@components/theme";
 import Layout from "@components/layout";
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
