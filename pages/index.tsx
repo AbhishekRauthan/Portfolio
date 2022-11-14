c25fc9cf64e49e93b726a8b2b172e3e1819354f3
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-11-14 11:26:02 +0530

    The following was done:
    	- created a projects file to handle projects page.
    	- implemented a small about section introducing myself as well as a heading with a byline.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,81 @@
-import { Stack, Heading, Icon, Box, Img } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import { IoMdHand } from "react-icons/io";
+import {
+  Box,
+  useColorModeValue,
+  Text,
+  Heading,
+  VStack,
+} from "@chakra-ui/react";
+import NextHead from "next/head";
 
 const HomePage = () => {
   return (
-    <Stack
-      as="section"
-      height={{ base: "90vh" }}
-      maxW="container.xl"
-      marginX="auto"
-      direction={["column", "row"]}
-    >
-      <Box
-        marginTop={{ base: "7" }}
+    <>
+      <NextHead>
+        <title>Abhishek - Front-End Developer | Data Science Enthusiast</title>
+      </NextHead>
+      <VStack
+        marginTop={{ base: "10" }}
+        marginBottom={{ base: "7" }}
         marginX="auto"
-        display="inline-flex"
+        display="flex"
+        spacing={{ base: "5" }}
+        paddingX="4"
         alignContent="baseline"
         textAlign="center"
+        flexDirection={{ base: "column" }}
       >
         <Heading
           as="h1"
-          paddingX="4"
           fontFamily="brandTitle"
-          fontSize="xl"
-          fontWeight="normal"
+          fontSize={{ base: "2xl" }}
+          fontWeight="bold"
+          letterSpacing={{ base: "wider" }}
         >
-          Hi{" "}
-          <motion.div
-            style={{ display: "inline-block", width: "max-content" }}
-            initial={{ rotate: 0 }}
-            animate={{ rotate: -20 }}
-            exit={{ rotate: -20 }}
-            transition={{
-              duration: 0.2,
-              yoyo: 10,
-            }}
-          >
-            <Icon as={IoMdHand} color="#f59e0b" boxSize={{ base: "7" }} />
-          </motion.div>
-          ,I am a Front-End Developer based in New-Delhi, India
+          Front-End Developer and a Data Science Enthusiast
         </Heading>
-      </Box>
-      <Box paddingTop={{base:"3.5"}}>
-        <Img
-          boxSize={{ base: "24" }}
-          src="/assets/cod (1).svg"
-          alt="My Avatar SVG"
-          rounded="full"
-          marginX="auto"
-        />
-      </Box>
-    </Stack>
+        <Text
+          fontFamily="brandBody"
+          fontSize={{ base: "xl" }}
+          paddingX={{ base: "3" }}
+          letterSpacing={{ base: "wide" }}
+        >
+          I design and code beautifull solutions for the web
+        </Text>
+      </VStack>
+      <VStack
+        as="section"
+        spacing={{ base: "3" }}
+        bgColor={useColorModeValue("brandBlue500", "brandCyan700")}
+        marginTop={{ base: "0", lg: "16" }}
+        paddingTop={{ base: "4", md: "7", lg: "16" }}
+        paddingBottom={{ base: "10",lg:"40" }}
+        textAlign={{ base: "center" }}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        color="white"
+        paddingX={{ base: "2", lg: "10" }}
+      >
+        <Heading as="h1" fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}>
+          Hi, I&apos;m Abhishek.{" "}
+          <Box as="br" display={{ base: "inline-block", md: "none" }} /> Great
+          to see you here!
+        </Heading>
+        <Text
+          as="p"
+          fontSize={{ base: "lg", md: "xl", lg: "2xl" }}
+          fontFamily="brandBody"
+          letterSpacing={{ base: "tight", md: "normal" }}
+          maxWidth={{ md: "container.md", lg: "container.lg" }}
+        >
+          I&apos;m a React Typescript developer with knowledge on NodeJS based
+          back-end technologies, based in New Delhi, India. I&apos;m passionate
+          about learning new things and solving problems. I love coding and
+          making cool things with it.
+        </Text>
+      </VStack>
+    </>
   );
 };
 
---
+++ pages/projects.tsx
@@ -0,0 +1,15 @@
+import NextHead from "next/head";
+import { Heading } from "@chakra-ui/react";
+
+const Projects = () => {
+  return (
+    <>
+      <NextHead>
+        <title>Abhishek - Projects</title>
+      </NextHead>
+      <Heading>Projects Page</Heading>
+    </>
+  );
+};
+
+export default Projects;
